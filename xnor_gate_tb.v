9cbcb109509fa87d436ae89f668978ad5d1c7226
krishan1707 <sonukumarss598512@ce.du.ac.in>
2025-11-19 22:15:44 +0530

    Add testbench for XNOR gate

---
+++ xnor_gate_tb.v
@@ -0,0 +1,20 @@
+`timescale 1ns/1ps
+module xnor_gate_tb();
+
+reg a, b;
+wire y;
+
+xnor_gate uut(a, b, y);
+
+initial begin
+    $dumpfile("xnor_gate.vcd");
+    $dumpvars(0, xnor_gate_tb);
+
+    a=0; b=0;
+    #10 a=0; b=1;
+    #10 a=1; b=0;
+    #10 a=1; b=1;
+    #10 $finish;
+end
+
+endmodule
